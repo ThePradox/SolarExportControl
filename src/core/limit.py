9018d49527bf2fafd18c244a68a9a50ce9687368
Pradox <li-pub@posteo.de>
2022-11-23 17:47:54 +0100

    refactoring

--- src/core/limit.py
+++ src/core/limit.py
@@ -15,26 +15,28 @@ from datetime import datetime
 # command: value of limit in watts or percent, decided by config.command.type
 
 class LimitCalculatorResult:
-    def __init__(self, reading: float, sample: float) -> None:
+    def __init__(self, reading: float, sample: float, overshoot: float, limit: float, command: float | None,
+                 is_calibration: bool, is_throttled: bool, is_not_over_min_diff: bool, is_retransmit: bool, elapsed: float) -> None:
         self.reading: float = reading
         self.sample: float = sample
-        self.overshoot: float | None = None
-        self.limit: float | None = None
-        self.command: float | None = None
-        self.is_calibration: bool = False
-        self.is_throttled: bool = False
-        self.is_not_over_min_diff: bool = False
-        self.is_retransmit: bool = False
+        self.overshoot: float = overshoot
+        self.limit: float = limit
+        self.command: float | None = command
+        self.is_calibration: bool = is_calibration
+        self.is_throttled: bool = is_throttled
+        self.is_not_over_min_diff: bool = is_not_over_min_diff
+        self.is_retransmit: bool = is_retransmit
+        self.elapsed: float = elapsed
 
 
 class LimitCalculator:
     def __init__(self, config: appconfig.AppConfig) -> None:
         self.config: appconfig.AppConfig = config
-        self.last_limit_time: datetime = datetime.min
+        self.last_command_time: datetime = datetime.min
         self.last_limit_value: float = config.command.min_power
-        self.last_command_has: bool = False
-        self.command_max: float = config.command.max_power
-        self.command_min: float = config.command.min_power
+        self.last_limit_has: bool = False
+        self.limit_max: float = config.command.max_power
+        self.limit_min: float = config.command.min_power
 
         deqSize: int = self.config.reading.smoothingSampleSize if self.config.reading.smoothingSampleSize > 0 else 1
 
@@ -58,77 +60,68 @@ class LimitCalculator:
         return r
 
     def __add_reading(self, reading: float) -> LimitCalculatorResult:
-        sample = self.__sampleReading(reading)
-        result = LimitCalculatorResult(reading, sample)
+        is_calibration = False
+        is_throttled = False
+        is_not_over_min_diff = False
+        is_retransmit = False
 
-        if not self.last_command_has:
+        sample = self.__sampleReading(reading)
+        
+        if not self.last_limit_has:
             logging.debug(f"First reading. Calibrate...")
             self.last_limit_value = self.__get_calibration()
-            self.last_command_has = True
-            result.is_calibration = True
+            self.last_limit_has = True
+            is_calibration = True
             logging.debug(f"Calibration value is: {self.last_limit_value}")
 
-            if self.config.command.type == appconfig.InverterCommandType.RELATIVE:
-                result.limit = self.__convert_to_relative_command(self.last_limit_value)
-            else:
-                result.limit = self.last_limit_value
-            return result
-
-        elapsed = round((datetime.now() - self.last_limit_time).total_seconds(), 2)
-        logging.debug(f"Elapsed since last command: {elapsed}")
-
-        if elapsed < self.config.command.throttle:
-            logging.debug("Throttle kicked in")
-            result.is_throttled = True
-            return result
-
+        elapsed = round((datetime.now() - self.last_command_time).total_seconds(), 2)
         overshoot = self.__convert_reading_to_relative_overshoot(sample)
-        result.overshoot = overshoot
         limit = self.__convert_overshot_to_limit(overshoot)
 
-        if limit is None:
-            return result
+        # Ignore conditions on calibration
+        if not is_calibration:
 
-        if self.config.command.retransmit > 0 and elapsed >= self.config.command.retransmit:
-            logging.debug("Forced retransmit kicked in. Ignoring mindiff")
-            result.is_retransmit = True
-        else:
-            if not self.__limit_is_min_diff(limit):
-                logging.debug("Limit is not over min diff")
-                result.is_not_over_min_diff = True
-                return result
+            # Check if command must be throttled
+            if elapsed < self.config.command.throttle:
+                is_throttled = True
 
-        result.limit = limit
-        command: float
+            # Ignore mindiff when retransmit > elapsed
+            elif self.config.command.retransmit > 0 and elapsed >= self.config.command.retransmit:
+                is_retransmit = True
 
-        if self.config.command.type == appconfig.InverterCommandType.RELATIVE:
-            command = self.__convert_to_relative_command(limit)
-        else:
-            command = limit
+            # Check for mindiff
+            elif not self.__limit_is_min_diff(limit):
+                is_not_over_min_diff = True
 
-        result.command = command
-        self.last_limit_time = datetime.now()
-        self.last_limit_value = limit
+        command: float | None = None
 
-        return result
+        if not (is_throttled or is_not_over_min_diff):
+            command = self.__convert_to_command(limit)
+            self.last_command_time = datetime.now()
+            self.last_limit_value = limit
+
+        return LimitCalculatorResult(reading=reading,
+                                     sample=sample,
+                                     overshoot=overshoot,
+                                     limit=limit,
+                                     command=command,
+                                     is_calibration=is_calibration,
+                                     is_throttled=is_throttled,
+                                     is_not_over_min_diff=is_not_over_min_diff,
+                                     is_retransmit=is_retransmit,
+                                     elapsed=elapsed)
 
     def get_command_min(self) -> float:
-        if self.config.command.type == appconfig.InverterCommandType.RELATIVE:
-            return self.__convert_to_relative_command(self.command_min)
-        else:
-            return self.command_min
+        return self.__convert_to_command(self.limit_min)
 
     def get_command_max(self) -> float:
-        if self.config.command.type == appconfig.InverterCommandType.RELATIVE:
-            return self.__convert_to_relative_command(self.command_max)
-        else:
-            return self.command_max
+        return self.__convert_to_command(self.limit_max)
 
     def reset(self) -> None:
         self.__samples.clear()
-        self.last_limit_time: datetime = datetime.min
+        self.last_command_time: datetime = datetime.min
         self.last_limit_value: float = float(0)
-        self.last_command_has: bool = False
+        self.last_limit_has: bool = False
         logging.debug("Limit context was reseted")
 
     def __get_calibration(self) -> float:
@@ -139,7 +132,7 @@ class LimitCalculator:
         except Exception as ex:
             logging.warning(f"customize.calibrate threw exception: {ex}")
 
-        return float(self.command_max)
+        return float(self.limit_max)
 
     def __get_smoothing_avg(self, reading: float) -> float:
         self.__samples.append(reading)
@@ -152,22 +145,22 @@ class LimitCalculator:
     def __convert_reading_to_relative_overshoot(self, reading: float) -> float:
         return (self.config.command.target - reading) * -1
 
-    def __convert_overshot_to_limit(self, overshoot: float) -> float | None:
-        if overshoot == 0:
-            return None
-        else:
-            return self.__cap_limit(self.last_limit_value + overshoot)
+    def __convert_overshot_to_limit(self, overshoot: float) -> float:
+        return self.__cap_limit(self.last_limit_value + overshoot)
 
     def __limit_is_min_diff(self, limit: float) -> bool:
         if self.config.command.min_diff == 0:
             return True
         return abs(self.last_limit_value - limit) >= self.config.command.min_diff
 
-    def __convert_to_relative_command(self, limit: float) -> float:
-        return (limit / self.command_max) * 100
+    def __convert_to_command(self, limit: float) -> float:
+        if self.config.command.type == appconfig.InverterCommandType.RELATIVE:
+            return (limit / self.limit_max) * 100
+        else:
+            return limit
 
     def __cap_limit(self, limit: float) -> float:
-        return max(self.command_min, min(self.command_max, limit))
+        return max(self.limit_min, min(self.limit_max, limit))
 
     @staticmethod
     def __log_result(result: LimitCalculatorResult) -> None:
@@ -177,19 +170,18 @@ class LimitCalculator:
         seg = []
         seg.append(f"Reading: {result.reading:>8.2f}")
         seg.append(f"Sample: {result.sample:>6.2f}")
-
-        if result.overshoot is not None:
-            seg.append(f"Overshoot: {result.overshoot:>6.2f}")
-
-        if result.limit is not None:
-            seg.append(f"Limit: {result.limit:>7.2f}")
+        seg.append(f"Overshoot: {result.overshoot:>6.2f}")
+        seg.append(f"Limit: {result.limit:>7.2f}")
 
         if result.command is not None:
-            seg.append(f"Limit: {result.command:>7.2f}")
+            seg.append(f"Command: {result.command:>7.2f}")
+        else:
+            seg.append(f"Command:    None")
 
         seg.append(f"Cal: {int(result.is_calibration)}")
         seg.append(f"Thr: {int(result.is_throttled)}")
         seg.append(f"Min: {int(result.is_not_over_min_diff)}")
         seg.append(f"Ret: {int(result.is_retransmit)}")
+        seg.append(f"Elapsed: {result.elapsed:.2f}")
 
         logging.debug(" | ".join(seg))
